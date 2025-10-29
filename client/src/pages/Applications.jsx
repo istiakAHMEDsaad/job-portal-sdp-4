d80592c3c17ff8e88c1b8cce0b272104578cddbe
husna_tajrin <husnajahantajrin@gmail.com>
2025-10-30 02:06:09 +0600

    testing branch

--- client/src/pages/Applications.jsx
+++ client/src/pages/Applications.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Applications = () => {
   return (
-    <div>Applications</div>
+    <div>
+      Applications
+      <p>this is application page</p>
+
+    </div>
   )
 }
 
