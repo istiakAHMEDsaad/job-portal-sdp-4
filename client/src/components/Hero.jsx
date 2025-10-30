7de8cd9c7d9174588e13e91d25ca18a82bf02953
istiakAHMEDsaad <istik2020@gmail.com>
2025-10-30 14:18:00 +0600

    company logo section

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -43,6 +43,43 @@ const Hero = () => {
           </button>
         </div>
       </div>
+
+      {/* company logo section */}
+      <div className='border border-gray-300 shadow-md mx-2 mt-5 p-6 rounded-md flex'>
+        <div className="flex justify-center gap-10 lg:gap-16 flex-wrap">
+          <p className="font-medium">Trusted by</p>
+          <img
+            className='h-6'
+            src={assets.microsoft_logo}
+            alt={assets.microsoft_logo}
+          />
+          <img
+            className='h-6'
+            src={assets.walmart_logo}
+            alt={assets.walmart_logo}
+          />
+          <img
+            className='h-6'
+            src={assets.accenture_logo}
+            alt={assets.accenture_logo}
+          />
+          <img
+            className='h-6'
+            src={assets.samsung_logo}
+            alt={assets.samsung_logo}
+          />
+          <img
+            className='h-6'
+            src={assets.amazon_logo}
+            alt={assets.amazon_logo}
+          />
+          <img
+            className='h-6'
+            src={assets.adobe_logo}
+            alt={assets.adobe_logo}
+          />
+        </div>
+      </div>
     </div>
   );
 };
