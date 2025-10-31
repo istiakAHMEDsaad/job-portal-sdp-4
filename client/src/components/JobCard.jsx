b9c921e9966864a654d15eaa90991a68a6c3b9dc
istiakAHMEDsaad <istik2020@gmail.com>
2025-10-31 23:38:36 +0600

    update jobs card layout

---
+++ client/src/components/JobCard.jsx
@@ -0,0 +1,29 @@
+import { assets } from "../assets/assets";
+
+const JobCard = ({ job }) => {
+  return (
+    <div>
+      <div>
+        <img src={assets.company_icon} alt='company logo' />
+      </div>
+
+      <h4>{job.title}</h4>
+
+      <div>
+        <span>{job.location}</span>
+        <span>{job.level}</span>
+      </div>
+
+      <p
+        dangerouslySetInnerHTML={{ __html: job.description.slice(0, 150) }}
+      ></p>
+
+      <div>
+        <button>Apply Now</button>
+        <button>Learn More</button>
+      </div>
+    </div>
+  );
+};
+
+export default JobCard;
