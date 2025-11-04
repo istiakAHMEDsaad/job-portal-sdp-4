1b7f195077b0e292af398f74b99443fdba41253a
istiakAHMEDsaad <istik2020@gmail.com>
2025-11-05 01:46:40 +0600

    added description pages header part

---
+++ client/src/components/Loading.jsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='w-12 md:w-14 h-12 md:h-14 border-4 border-gray-300 border-t-4 border-t-blue-400 rounded-full animate-spin'></div>
+    </div>
+  );
+};
+
+export default Loading;
