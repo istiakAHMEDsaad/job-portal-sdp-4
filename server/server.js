7a43553878c5a9a59c1427e43d177f9fa3d1255d
istiakAHMEDsaad <istik2020@gmail.com>
2025-11-11 19:44:56 +0600

    added company controller

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import "dotenv/config";
 import connectDB from "./config/db.js";
 import * as Sentry from "@sentry/node";
 import { clerkWebhooks } from "./controllers/webhooks.js";
+import companyRoutes from "./routes/companyRoutes.js";
 
 // Initialize Express
 const app = express();
@@ -26,6 +27,7 @@ app.get("/debug-sentry", function mainHandler(req, res) {
 });
 
 app.post("/webhooks", clerkWebhooks);
+app.use("/api/company", companyRoutes);
 
 // Server start
 const PORT = process.env.PORT || 5000;
