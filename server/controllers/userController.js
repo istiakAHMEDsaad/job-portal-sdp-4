761d8402f600cf0a4673d01497fdf6130794c6d8
istiakAHMEDsaad <istik2020@gmail.com>
2025-11-13 20:24:03 +0600

    added user controller and routes

---
+++ server/controllers/userController.js
@@ -0,0 +1,25 @@
+// Get user data
+export const getUserData = async (req, res) => {
+  
+};
+
+// Apply for a job
+export const applyForJob = async (req, res) => {};
+
+// Get user applied application
+export const getUserJobApplications = async (req, res) => {};
+
+// Update user profile (resume)
+export const updateUserResume = async (req, res) => {};
+
+// Get user info
+export const userInfo = async (req, res) => {};
+
+// Post user info
+export const postUserInfo = async (req, res) => {};
+
+// Get job experience
+export const getJobExperience = async (req, res) => {};
+
+// Post job experience
+export const postUserExperience = async (req, res) => {};
---
+++ server/routes/userRoutes.js
@@ -0,0 +1,40 @@
+import express from "express";
+import {
+  applyForJob,
+  getJobExperience,
+  getUserData,
+  getUserJobApplications,
+  postUserExperience,
+  postUserInfo,
+  updateUserResume,
+  userInfo,
+} from "../controllers/userController.js";
+import upload from "../controllers/multer.js";
+
+const router = express.Router();
+
+// Get user data
+router.get("/user", getUserData);
+
+// Apply for a job
+router.post("/apply", applyForJob);
+
+// Get applied jobs data
+router.get("/applications", getUserJobApplications);
+
+// Update user profile (resume)
+router.post("/update-resume", upload.single("resume"), updateUserResume);
+
+// Get user info
+router.get("/user-info", userInfo);
+
+// Post user info data
+router.post("/post-user-info", postUserInfo);
+
+// Get job experience
+router.get("/job-experieence", getJobExperience);
+
+// Post job experience
+router.post("/post-job-experience", postUserExperience);
+
+export default router;
