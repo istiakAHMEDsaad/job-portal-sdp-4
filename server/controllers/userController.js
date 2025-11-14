e203e49341ca435444320b664c955bee0309fec0
istiakAHMEDsaad <istik2020@gmail.com>
2025-11-14 17:48:36 +0600

    get user applied application

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,13 +1,84 @@
+import Job from "../models/Job.js";
+import JobApplication from "../models/JobApplication.js";
+import User from "../models/User.js";
+
 // Get user data
 export const getUserData = async (req, res) => {
-  
+  const userId = req.auth.userId;
+
+  try {
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User Not Found" });
+    }
+
+    res.json({ success: true, user });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
 };
 
 // Apply for a job
-export const applyForJob = async (req, res) => {};
+export const applyForJob = async (req, res) => {
+  const { jobId } = req.body;
+
+  const userId = req.auth.userId;
+
+  try {
+    const isAlreadyApplied = await JobApplication.find({ jobId, userId });
+
+    if (isAlreadyApplied.length > 0) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Already Applied!" });
+    }
+
+    const jobData = await Job.findById(jobId);
+
+    if (!jobData) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Job Not Found!" });
+    }
+
+    await JobApplication.create({
+      companyId: jobData.companyId,
+      userId,
+      jobId,
+      date: Date.now(),
+    });
+
+    res.status(200).json({ success: true, message: "Applied Successfully" });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
 
 // Get user applied application
-export const getUserJobApplications = async (req, res) => {};
+export const getUserJobApplications = async (req, res) => {
+  try {
+    const userId = req.auth.userId;
+
+    const applications = await JobApplication.find({ userId })
+      .populate("companyId", "name email image")
+      .populate("jobId", "title description location category level salary")
+      .exec();
+
+    if (!applications) {
+      return res.status(400).json({
+        success: false,
+        message: "No job applications found for this user",
+      });
+    }
+
+    return res.status(200).json({ success: true, applications });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
 
 // Update user profile (resume)
 export const updateUserResume = async (req, res) => {};
